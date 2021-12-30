0d09127d9d33dccd8a1c20be847c7e788c3cbb44
高巍 <gaowei6167@dingtalk.com>
2021-12-30 10:00:45 +0800

    gaowei update 整理春节特辑

--- 新年特辑/新年特辑/订单统计.py
+++ 新年特辑/新年特辑/订单统计.py
@@ -3,13 +3,17 @@ from pathlib import Path, PurePath
 from collections import defaultdict
 
 # 订单路径
-download_path = '/Users/edz/Desktop/效率专栏/新年特辑/订单'
+download_path = 'python_productivity\新年特辑\新年特辑\订单'
 
 # 取得该目录下所有的xlsx格式文件
 p = Path(download_path)
 files = [x for x in p.iterdir() if PurePath(x).match('*.xlsx')]
 
 # 定义字典用于结果统计
+'''
+这里的defaultdict(function_factory)构建的是一个类似dictionary的对象，其中keys的值，自行确定赋值，但是values的类型，是function_factory的类实例，而且具有默认值。
+比如default(int)则创建一个类似dictionary对象，里面任何的values都是int的实例，而且就算是一个不存在的key, d[key] 也有一个默认值，这个默认值是int()的默认值0.
+'''
 total = defaultdict(int)
 
 # 中文做字典的key会有问题,做两个简单的翻译函数
@@ -28,6 +32,8 @@ def dict_name_eng2chi(key):
 
 # 遍历文件 
 for file in files:
+    # 也可以用openpyxl代替xlrd打开.xlsx文件：
+    # df=pandas.read_excel(‘data.xlsx’,engine=‘openpyxl’)
     sheet = xlrd.open_workbook(file)
 
     # 遍历表格
@@ -43,6 +49,9 @@ for file in files:
 
             # 统计每种水果的销售额           
             fruit_name = dict_trans_chi2eng(fruit[0])[0]
+            # print(fruit_name)
+            # print(total[fruit_name])
+            # print(total)
             total[fruit_name] = total[fruit_name] + fruit[-2]
 
 for fruit_name in total:
