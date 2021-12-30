33954902c3b3c1f3fc6ddac83b32f9d3b986f342
高巍 <gaowei6167@dingtalk.com>
2021-12-30 14:44:53 +0800

    gaowei update 调整第八章代码

--- 文章8代码/正则表达式.py
+++ 文章8代码/正则表达式.py
@@ -8,7 +8,7 @@ len11 = len(s)==11
 print(f'字符串是否是数字 {digit}')
 print(f'字符串的长度是否为11个 {len11}')
 
-
+# Python 的正则表达式库
 import re
 
 re.search("[0-9]{11}","13855556666")
@@ -18,9 +18,13 @@ re.search("[0-9]{11}","13855556666")
 points = re.search(".....","aaa13855557890bbb")
 print(points)
 
+# group函数提取搜索结果
 yesno = re.search("(Y|y)(es)*","aayesbb").group(0)
 print(f'yesno is {yesno}')
 
 # 将字符串中的yes替换为no
 print(re.sub("(Y|y)(es)*", "No","aayesbb"))
 
+
+re.search(".....", "aaa13855557890bbb")
+re.search(".{5}", "aaa13855557890bbb") # 也可以这样写
